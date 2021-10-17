090cb18980689fbbca1f7d78db8603ff524def83
peterabel25 <peterabel2504@gmail.com>
2021-10-17 21:43:11 +0300

    first commit

---
+++ myscript.js
@@ -0,0 +1,149 @@
+//declaring variables
+
+/*
+   var age;
+   let umri;
+   const Pi;
+
+
+   */
+
+
+//conditional  code
+  /* 
+  if(age<19){};
+
+if (age>20) {
+    document.write("you are young")
+} else {
+    alert("go take the covid vaccine")
+ }
+
+ */
+
+
+// operators
+
+  /*addition
+  subtraction
+  multiplication
+  division
+  modulus
+*/
+
+//operator precedence
+
+/*
+  a+=10;
+  a=a+10;
+
+*/
+
+//comparison
+  /*
+  >=
+  <=
+  ==
+===
+*/
+
+//modulus operator
+   //gives the remainder of the division 4%3=1
+ 
+//increment and decrement
+  //a++,++a
+  //a--,--a
+
+  //ternary operators
+    //condition?true:false;
+
+    //looping through various elements
+      /*for(var i=0;i<10;i++){};
+      var i;
+      while(i<10);
+      do{}
+      while{}
+       */
+
+
+//dealing with functions
+ /* function addition(x,Y){
+     let sum = x+Y;
+     return sum;
+ }
+*/
+  //calling a function
+/*
+   let result = addition(10,20);
+  
+ document.write(result);
+ */
+
+
+
+ //dealing with arrays
+
+ /*
+    let cars=[
+      ' volvo',
+      'bmw',
+      'nissan',
+      'toyota'
+  ];
+
+  document.write(cars[1]);
+
+  */
+
+
+
+//objects
+   
+   //defining an object
+    /*let student={
+      name:"peter",
+      age:21,
+      religion:"christian",
+      ethinicity:"black",
+      height:"5.5 ft",
+      fullname:function(){
+        return(this.name+"  "+this.name) 
+      }
+    };
+ 
+
+//accessing the object's properties
+  document.write(student.name+"<br>");
+  document.write(student.fullname());
+
+
+*/
+
+  
+
+/* let currentDate = new Date();
+ document.write(currentDate.getDay() +"<br>");
+ document.write(currentDate.getMonth() +"<br>");
+ document.write(currentDate.getFullYear() +"<br>");
+*/
+
+
+/* 
+let button = document.getElementById("btn");
+button.onclick = function(){
+  alert("hellooooo");
+}
+
+*/
+
+
+document.cookie="username=peter;expires=4:50;"
+
+alert(document.cookie);
+
+
+
+
+
+
+
